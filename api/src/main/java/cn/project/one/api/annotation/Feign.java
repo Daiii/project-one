0f76d7907fb6a3048709d1ea2536c1e20d500ec9
zhangbo <zhangbo@hzzl.net>
2023-08-04 15:49:43 +0800

    project-one

--- api/src/main/java/cn/project/one/api/annotation/Feign.java
+++ api/src/main/java/cn/project/one/api/annotation/Feign.java
@@ -4,9 +4,10 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@java.lang.annotation.Target(ElementType.TYPE)
+@Target(ElementType.TYPE)
 @Documented
 public @interface Feign {
 
