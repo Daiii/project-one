5fbe9255f0455a190235964c0159967715f447a2
zhangbo <zhangbo@hzzl.net>
2023-12-27 09:50:16 +0800

    project-one

--- common/src/main/java/cn/project/one/common/constants/ResultCodeEnum.java
+++ common/src/main/java/cn/project/one/common/constants/ResultCodeEnum.java
@@ -1,22 +1,14 @@
 package cn.project.one.common.constants;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ResultCodeEnum {
 
     OK(0, ""), SUCCESS(200, ""), NOT_FOUND(404, "Not Found");
 
-    private int code;
-    private String message;
-
-    ResultCodeEnum(int code, String message) {
-        this.code = code;
-        this.message = message;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    private final int code;
+    private final String message;
 }
