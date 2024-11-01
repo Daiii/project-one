c52bfaef86100345251299e8c7a4e4e715b4dd69
zhangbo <zhangbo@hzzl.net>
2024-11-01 12:33:26 +0800

    project-one

--- core/src/main/java/cn/project/one/core/listener/ProjectOneRefreshedListener.java
+++ core/src/main/java/cn/project/one/core/listener/ProjectOneRefreshedListener.java
@@ -1,12 +1,10 @@
 package cn.project.one.core.listener;
 
-import org.springframework.context.EnvironmentAware;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import cn.hutool.core.convert.Convert;
 import cn.hutool.core.thread.ThreadUtil;
 import cn.project.one.common.Node;
 import cn.project.one.common.util.InetUtil;
@@ -21,10 +19,14 @@ import lombok.RequiredArgsConstructor;
  */
 @Component
 @RequiredArgsConstructor
-public class ProjectOneRefreshedListener implements EnvironmentAware {
+public class ProjectOneRefreshedListener {
 
-    private Environment environment;
     private final AbstractServiceRegistry serviceRegistry;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+    @Value("${server.port}")
+    private Integer serverPort;
 
     @EventListener(ContextRefreshedEvent.class)
     public void contextRefreshedEvent(ContextRefreshedEvent event) {
@@ -36,16 +38,9 @@ public class ProjectOneRefreshedListener implements EnvironmentAware {
      * 注册服务
      */
     private void registerNode() {
-        String name = environment.getProperty("spring.application.name");
         String address = InetUtil.getHost();
-        int port = Convert.toInt(environment.getProperty("server.port"), 8080);
         String id = InetUtil.getHost();
-        Node node = new Node(id, name, address, port);
+        Node node = new Node(id, applicationName, address, serverPort);
         serviceRegistry.register(node);
-    }
-
-    @Override
-    public void setEnvironment(Environment environment) {
-        this.environment = environment;
     }
 }
